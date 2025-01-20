5a4a1ff9082fa8549a5fb1c579aa1dbd7fe68b13
RuiGene <zrdanielyang@gmail.com>
2025-01-20 20:03:23 +1300

    Established radar chart data

--- pages/NBA.py
+++ pages/NBA.py
@@ -5,6 +5,6 @@ import matplotlib.pyplot as plt
 
 st.set_page_config(page_title="NBA Player Profile", page_icon = "🏀")
 
-col1, col2, col3 = st.columns(3)
+# col1, col2, col3 = st.columns(3)
 
 text_search = st.text_input("Search for an NBA player", value="")
